e205bb66946cd351108da32c6d71bb262341133f
Roman <162383532+RomanDmitrov@users.noreply.github.com>
2026-07-04 20:56:19 +0100

    fix schemas

--- schemas.py
+++ schemas.py
@@ -2,24 +2,22 @@ from pydantic import BaseModel
 from typing import Optional
 
 
-
 class BookingNotification(BaseModel):
     """
-        Данные заявки, которые присылает Django при создании
-        нового booking request или partnership request.
+    Данные заявки, которые присылает Django при создании
+    нового booking request или partnership request.
     """
     request_type: str
     name: str
     email: Optional[str] = None
     phone: Optional[str] = None
     telegram: Optional[str] = None
-    event_date: Optional[str] = None
     message: Optional[str] = None
 
     # Поля только для booking (заявка на бронювання)
-    event_type: Optional[str] = None  # Тип івенту
-    guests_count: Optional[int] = None  # Кількість гостей
-    budget: Optional[int] = None  # Бюджет
+    event_type: Optional[str] = None
+    guests: Optional[int] = None
+    budget: Optional[str] = None
 
     # Поле только для partnership (заявка на партнерство)
-    cooperation_type: Optional[str] = None  # Тип співпраці
+    partnership_type: Optional[str] = None
